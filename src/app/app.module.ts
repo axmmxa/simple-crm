26502ada17f26f355713638991ed1be7f28a7591
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-02-15 16:14:51 +0100

    add use button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIcon, MatIconModule} from '@angular/material/icon';
 import { DahsboardComponent } from './dahsboard/dahsboard.component';
@@ -23,7 +24,9 @@ import { UserComponent } from './user/user.component';
     MatToolbarModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+constructor() {
+
+
+
+}
+
+addUser() {
+    console.log("add new user")
+}
+
 }
