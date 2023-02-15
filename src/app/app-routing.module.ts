34919d7cc73219faad5750e9b3209df5ac7dc530
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-02-15 15:47:04 +0100

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DahsboardComponent } from './dahsboard/dahsboard.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component:DahsboardComponent},
+  {path: 'dashboard' , component:DahsboardComponent},
+  {path: 'user' , component:UserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
