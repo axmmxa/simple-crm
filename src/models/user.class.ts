d0893a60039e4b4eb7c3bc2ada9b24c0d57d5600
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-02-16 11:48:17 +0100

    add model user

---
+++ src/models/user.class.ts
@@ -0,0 +1,14 @@
+export class User {
+    firstName: string;
+    lastName: string;
+    birthDate: number;
+    street: string;
+    zipCode: number;
+    city: string;
+
+    constructor(obj?: any) {
+        if(obj) {
+            this.firstName = obj.firstName;
+        }
+    }
+}
