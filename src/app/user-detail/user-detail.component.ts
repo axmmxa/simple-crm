1b37c7e09982e3d020ffe6888d9cf7cf1d8067e8
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-02-18 13:36:21 +0100

    get user id from firestore

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent {
+
+  userId = ''
+
+  constructor(private route:ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.userId = paramMap.get('id');
+      console.log("GOT ID", this.userId)
+  })
+  }
 
 }
