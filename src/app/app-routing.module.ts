8715f827753d5e239be78a1ff28ff26d29a333ed
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-02-18 12:26:48 +0100

    user id routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DahsboardComponent } from './dahsboard/dahsboard.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserComponent } from './user/user.component';
+
 
 const routes: Routes = [
   {path: '' , component:DahsboardComponent},
   {path: 'dashboard' , component:DahsboardComponent},
   {path: 'user' , component:UserComponent},
+  {path: 'user/:id' , component:UserDetailComponent},
 ];
 
 @NgModule({
