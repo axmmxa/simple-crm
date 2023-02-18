8113efc2fea829300c1a7e5d3965292b1b8e9d8c
Alexander Much <alexandermuch@Christophs-Mini.fritz.box>
2023-02-18 13:54:57 +0100

    get user data from firebase for user detail

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { user } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,16 +12,33 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailComponent {
 
   userId = ''
+  user: User = new User;
 
-  constructor(private route:ActivatedRoute) {
+  constructor(private route:ActivatedRoute, private firestore: AngularFirestore) {
 
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( paramMap => {
       this.userId = paramMap.get('id');
+
       console.log("GOT ID", this.userId)
+
+      this.getUser()
+
   })
+  }
+
+  getUser() {
+    this.firestore
+    .collection('users')
+    .doc(this.userId)
+    .valueChanges()
+    .subscribe( (user:any) => {
+      this.user = new User(user)
+      
+      console.log("retrieved user", this.user)
+    })
   }
 
 }
